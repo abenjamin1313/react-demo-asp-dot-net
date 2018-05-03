8e61763dc66ae66d082014e5434ba59f920c577a
abenjamin1313 <abenjamin1313@gmail.com>
2018-05-03 11:49:23 -0400

    Added module to use babel-loader and presets: babel-preset-env AND babel-preset-react,

--- react-demo-app/Scripts/react/app.js
+++ react-demo-app/Scripts/react/app.js
@@ -1,1 +1,5 @@
 ﻿const x = "New feature of ES6.";
+
+const y = "New feature of ES6 is cool.";
+
+const v = "New feature t of ES6 is cool.";
--- react-demo-app/Scripts/react/webpack.config.js
+++ react-demo-app/Scripts/react/webpack.config.js
@@ -4,5 +4,20 @@
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js"
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env', 'babel-preset-react']
+                    }
+                }
+            }
+        ]
     }
 }
